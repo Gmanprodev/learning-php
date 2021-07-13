1d678dfd9b27bfadae0f573375500f00704d2d22
Gmanprodev <marcgulliver@marcs-mbp.home>
2021-07-13 15:23:26 +0100

    added navbar, mobile navbar and navlinks.

--- index.php
+++ index.php
@@ -1,25 +1,63 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>My First php</title>
-    <style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-      header {
-        background-color: #e3e3e3;
-        padding: 2em;
-        text-align: center;
-      }
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" 
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" type="text/css"/>
 
-    </style>
-  </head>
-  </body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" type="text/css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" type="text/css" />
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+    <title>To Do Application</title>
+</head>
+<body>
     <header>
-      <h1>
-        <?php
-          echo "Hello, World!";
-        ?>
-      </h1>
+        <div class="navbar-fixed">
+            <nav class="nav-extended text-shadow teal lighten-2">
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Task Manager</a>
+                    <a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i class="fas fa-bars"></i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li><a href="">Profile</a></li>
+                        <li><a href="">Tasks</a></li>
+                        <li><a href="">Add Tasks</a></li>
+                        <li><a href="">Log Out</a></li>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Log In</a></li>
+                        <li><a href="">Sign Up</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <ul class="sidenav teal lighten-2" id="mobile-demo">
+            <li><h4 class="center-align white-text text-darken-4 sidenav-header">Task Manager</h4></li>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Tasks</a></li>
+            <li><a href="">Add Tasks</a></li>
+            <li><a href="">Log Out</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Log In</a></li>
+            <li><a href="">Sign Up</a></li>
+        </ul>
     </header>
-  </body>
+    
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script src="script.js"></script>
+
+</body>
 </html>
+
