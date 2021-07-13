6029510dd28d8c1b2fa71cb5534111f70040245f
Gmanprodev <marcgulliver@marcs-mbp.home>
2021-07-13 11:27:03 +0100

    test commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My First php</title>
+    <style>
+
+      header {
+        background-color: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+      }
+
+    </style>
+  </head>
+  </body>
+    <header>
+      <h1>
+        <?php
+          echo "Hello, World!";
+        ?>
+      </h1>
+    </header>
+  </body>
+</html>
