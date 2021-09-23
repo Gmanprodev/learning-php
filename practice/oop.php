d02a8378fce251d02d24ad4a508769427c3fbb5d
Gmanprodev <marcgulliver@marcs-mbp.home>
2021-09-23 17:40:39 +0100

    added more practice code to oop file.

--- practice/oop.php
+++ practice/oop.php
@@ -106,3 +106,122 @@ class Car {
 // creating a new object and passing the new values.
 $car_sold = new Car("Volvo", "XC90", "Silver");
 
+
+
+
+// create a class with three different modifiers.
+class Person {
+    public $gender;
+    protected $height;
+    private $weight;
+}
+// create a new object of the person class.
+$james = new Person();
+// this property can be accessed as it's public.
+$james->gender = 'Male';
+// this property cannot be accessed because it is outside of the class.
+$james->height = '6ft, 2';
+// this property cannot be accessed because it is outside of the class.
+$james->weight = '12 stone';
+
+
+
+// create a class with three properties.
+class School {
+    public $name;
+    public $location;
+    public $ofsted;
+    // create three methods with three different modifiers.
+    function set_name($name) {
+        $this->name = $name;
+    }
+    protected function set_location($location) {
+        $this->location = $location;
+    }
+    private function set_ofsted($ofsted) {
+        $this->ofsted = $ofsted;
+    }
+}
+// create a new object of the school class.
+$find_school = new School();
+// this property can be accessed as it's default property modifier is public.
+$find_school->set_name("St Peters Primary School");
+// this property cannot be accessed because the method is using the protected modifier.
+$find_school->set_location("Billericay");
+// this property cannot be accessed because the method is using the private modifier.
+$find_school->set_ofsted("Outstanding");
+
+
+
+// create a class.
+class Vegetable {
+    // create class properties.
+    public $name;
+    public $color;
+    // create a public construct method which is called every time a new object is created.
+    public function __construct($name, $color) {
+        // declares the current object.
+        $this->name = $name;
+        $this->color = $color;
+    }
+    // create a public method which passes the object values and outputs is a string.
+    public function intro() {
+        echo "The vegetable is {$this->name} and the color is {$this->color}.";
+    }
+}
+// create a class which extends from the Vegetable class and uses it's properties and methods.
+class Cauliflower extends Vegetable {
+    // create a method which outputs a string.
+    public function message() {
+        echo "Am I a vegetable or a flower? ";
+    }
+}
+// create a new object of the Cauliflower class which also extends the methods and properties of the Vegetable class.
+$cauliflower = new Cauliflower("Cauliflower", "White");
+// calls the method from the Cauliflower class and outputs the string.
+$cauliflower->message();
+// calls the method from the Vegetable parent class and outputs the strings.
+$cauliflower->intro();
+
+
+
+
+
+// create a class.
+final class Ninja_Turtles {
+    // create class properties.
+    public $name;
+    public $color;
+    // create a public construct method which is called every time a new object is created.
+    public function __construct($name, $color, $fav_food) {
+        // declares the current object.
+        $this->name = $name;
+        $this->color = $color;
+        $this->food = $fav_food;
+    }
+    // create a public method which passes the object values and outputs is a string.
+    public function intro() {
+        echo "I am {$this->name} and I wear a {$this->color} headband.";
+    }
+}
+/* create a class which extends from the Ninja_Turtles class and uses it's properties and methods.
+This won't work because we've used the 'final' keyword which stops it from being extended. */
+class Leonardo extends Ninja_Turtles {
+    // create a method which outputs a string.
+    public function message() {
+        echo "My favourite food is {$this->food}.";
+    }
+}
+// create a new object of the Leonardo class which also extends the methods and properties of the Ninja_turtles class.
+$turtle = new Leonardo("Leonardo", "Blue", "Pizza");
+// calls the method from the Leonardo class and outputs the string.
+$turtle->message();
+// calls the method from the Ninja_turtles parent class and outputs the strings.
+$turtle->intro();
+
+
+
+
+
+
+
